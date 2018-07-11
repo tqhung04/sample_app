cee61dce0474f56b816cb5697b624d1293c6d03c
TQHUNG04 <tqhung04@gmail.com>
2018-07-11 10:34:38 +0700

    Chapter 3: Mostly static pages

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module SampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    I18n.load_path += Dir[Rails.root.join("config", "locales", "*.{rb,yml}")]
+    I18n.default_locale = :vi
+    I18n.available_locales = [:en, :vi]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
