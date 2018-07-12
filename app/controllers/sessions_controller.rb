d67c63dadb7d01a8e67246983df414897786a3df
TQHUNG04 <tqhung04@gmail.com>
2018-07-12 16:28:43 +0700

    Chapter 9: Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-
   def new
   end
 
@@ -7,6 +6,9 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
+
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+
       flash[:success] = t "login_success_msg"
       redirect_to root_url
     else
@@ -16,7 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,34 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: Settings.minimum.length_pass }
 
-  def User.digest string
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create string, cost: cost
-  end
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
 
-  def User.new_token
-    SecureRandom.urlsafe_base64
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
   def remember
     self.remember_token = User.new_token
     update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
   end
 
   private
