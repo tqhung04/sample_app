dec02d4abffdd094d3d40fd31aacce4824b4036c
TQHUNG04 <tqhung04@gmail.com>
2018-07-18 15:40:59 +0700

    Chapter 14: Following users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :getUserById, only: [:show, :edit, :update, :destroy, :correct_user]
+  before_action :logged_in_user, except: [:show, :new, :create]
+  before_action :getUserById, except: [:index, :new, :create, ]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
@@ -51,6 +51,18 @@ class UsersController < ApplicationController
     end
 
     redirect_to users_url
+  end
+
+  def following
+    @title = t("following")
+    @users = @user.following.paginate(page: params[:page]).per_page Settings.page.per_page
+    render :show_follow
+  end
+
+  def followers
+    @title = t("followers")
+    @users = @user.followers.paginate(page: params[:page]).per_page Settings.page.per_page
+    render :show_follow
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: Relationship.name, foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: Relationship.name, foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -62,7 +67,20 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = Relationship.where(follower_id: :user_id).select("followed_id")
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
+
+  def follow other_user
+    following << other_user
+  end
+
+  def unfollow other_user
+    following.delete other_user
+  end
+
+  def following? other_user
+    following.include? other_user
   end
 
   private
